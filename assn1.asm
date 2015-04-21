c4501129328e62a59f879af9a48d070b33a75f87
TstevetsT <stevets@hushmail.com>
2015-04-21 14:16:19 -0700

    update

--- assn1.asm
+++ assn1.asm
@@ -24,8 +24,10 @@ sub esp, 16   ;allocate local storage
 ;open file called flag   
 ; flag0 in little indian hex= 0x67 'g' 0x61 'a', 0x6c 'l', 0x66 'f', 
 ;     0x0 'null' 
-movsd [ebp+4], 0x67616c66
-movsb [ebp+8], 0x00
+mov eax, 0x67616c66
+mov [ebp+4], eax
+mov eax, 0x00000000
+mov [ebp+8], eax
 mov ecx, 0   ;mode=RDonly=0  WRonly=1  RDRW=2  
 mov ebx, [ebp+4]  ;name of file to open
 mov eax, 5    ;open syscall

